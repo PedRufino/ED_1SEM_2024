ea3f499c51e7120a28277cdf1e7e93ea8819e217
Pedro Neto <pedro.rmn19@gmail.com>
2024-03-02 10:22:06 -0300

    [Add]: Adicionando codigos da primeira lista de exercicios

--- lista01/credCliente.cpp
+++ lista01/credCliente.cpp
@@ -2,8 +2,7 @@
 #include <string>
 using namespace std;
 
-struct CredCliente
-{
+struct CredCliente {
     string nomeCompleto;
     string dataNasc;
     string cpf;
@@ -11,10 +10,12 @@ struct CredCliente
     int agenciaConta;
     int numeroConta;
     int tipoConta;
+    double valorCredito;
+    int numParcelas;
+    double valorParcela;
 };
 
-void inserirDados(CredCliente& cliente)
-{
+void inserirDados(CredCliente& cliente) {
     cout << "Nome Completo: ";
     cin >> cliente.nomeCompleto;
     cout << "Data de Nascimento: ";
@@ -29,21 +30,29 @@ void inserirDados(CredCliente& cliente)
     cin >> cliente.numeroConta;
     cout << "Tipo de Conta: ";
     cin >> cliente.tipoConta;
+    cout << "Valor do Crédito: ";
+    cin >> cliente.valorCredito;
+    cout << "Número de Parcelas (1 a 12): ";
+    cin >> cliente.numParcelas;
+
+    while (cliente.numParcelas < 1 || cliente.numParcelas > 12) {
+        cout << "Número de Parcelas inválido. Insira novamente (1 a 12): ";
+        cin >> cliente.numParcelas;
+    }
+
+    cliente.valorParcela = cliente.valorCredito / cliente.numParcelas;
 }
 
-int main()
-{
+int main() {
     const int qtd = 2;
     CredCliente vetor_pessoa[qtd];
 
-    for (int i = 0; i < qtd; i++)
-    {
+    for (int i = 0; i < qtd; i++) {
         cout << "Inserindo dados para a pessoa " << i + 1 << endl;
         inserirDados(vetor_pessoa[i]);
     }
 
-    for (int i = 0; i < qtd; i++)
-    {
+    for (int i = 0; i < qtd; i++) {
         cout << "" << endl;
         cout << "Dados da pessoa " << i + 1 << ":" << endl;
         cout << "Nome Completo: " << vetor_pessoa[i].nomeCompleto << endl;
@@ -53,8 +62,12 @@ int main()
         cout << "Agência da Conta: " << vetor_pessoa[i].agenciaConta << endl;
         cout << "Número da Conta: " << vetor_pessoa[i].numeroConta << endl;
         cout << "Tipo de Conta: " << vetor_pessoa[i].tipoConta << endl;
+        cout << "Valor do Crédito: " << vetor_pessoa[i].valorCredito << endl;
+        cout << "Número de Parcelas: " << vetor_pessoa[i].numParcelas << endl;
+        cout << "Valor da Parcela: " << vetor_pessoa[i].valorParcela << endl;
         cout << endl;
     }
 
     return 0;
 }
+
